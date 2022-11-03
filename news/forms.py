cfa37fb67f75d6474907e0e16b4655976858ac35
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2022-11-03 17:22:33 +0100

    setup comments and replies, social share

--- news/forms.py
+++ news/forms.py
@@ -4,19 +4,19 @@ from django import forms
 from news.models import Comment, Reply
 
 class CommentForm(forms.ModelForm):
-    name = forms.CharField()
-    email = forms.EmailField()
-    website = forms.URLField(required=False)
-    message = forms.CharField(widget=forms.Textarea())
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={"class": "form-control", "id": "name"}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control", "id": "email"}))
+    website = forms.URLField(required=False, widget=forms.URLInput(attrs={"class": "form-control", "id": "website"}))
+    message = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control", "cols": "30", "rows": "7", "id": "message"}))
 
     class Meta:
         model = Comment
         fields = ['name', 'email', 'website', 'message']
 
 class ReplyForm(forms.ModelForm):
-    name = forms.CharField()
-    email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea())
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={"class": "form-control", "id": "name"}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control", "id": "email"}))
+    message = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control", "cols": "30", "rows": "7", "id": "message"}))
 
     class Meta:
         model = Reply
